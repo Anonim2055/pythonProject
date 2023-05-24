38eca276f1455e01b330300b1eaf19957dd4f4e4
Yarin Bukai <72891249+byarin90@users.noreply.github.com>
2023-05-24 12:25:53 +0300

    add user

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 from flask import Flask,jsonify,request
-_id =2
-users_list = [
-    {
-        "id":1,
-        "name":"yarin",
-        "email":"byarin90@gmail.com"
-    },
-    {
-        "id":2,
-        "name":"david",
-        "email":"david@gmail.com"
-    }
-]
+from pymongo import MongoClient
+from bcrypt import hashpw,gensalt
+
 
 app = Flask(__name__)
 
-# READ
+try:
+    mongo = MongoClient(host="localhost",port=27018,serverSelectionTimeoutMS=5000)
+    db = mongo.flask
+
+    print("MongoDB Connect")
+except Exception:
+    print("Unable to connect MongoDB")
 
 
 @app.route('/',methods=['GET'])
@@ -29,77 +25,76 @@ def index():
 
 #READ
 
-
-@app.route('/users',methods=['GET'])
-def get_users():
-    resp = jsonify({
-       "users_list":users_list
-    })
-    return resp
-
-
-# CREATE
-
-
+#
+# @app.route('/users',methods=['GET'])
+# def get_users():
+#     resp = jsonify({
+#        "users_list":users_list
+#     })
+#     return resp
+#
+#
+# # CREATE
+#
+#
 @app.route('/users',methods=['POST'])
 def add_user():
     body = request.json
-    required_keys = {"name", "email"}
+    required_keys = {"name", "email","password"}
     # check if there's any invalid key in the request body
     if set(body.keys()) != required_keys:
         return jsonify({"error": "Invalid or missing field in request body"}), 400
         # return error message with 400 status code
 
+    password = body["password"].encode('utf-8')  # convert password to bytes
+    hashed_password = hashpw(password,gensalt())  # hash the password
 
-    _name = body['name']
-    _email = body['email']
-    body['id'] = _id + 1
-    users_list.append(body)
-    print(body)
+    result = db.users.insert_one({
+        "email":body["email"],
+        "name":body["name"],
+        "password":hashed_password.decode('utf-8')  # store hashed password as string
+    })
+
     resp = jsonify({
+        "id":str(result.inserted_id),
         "message":"user added"
     })
     return resp
-
-# UPDATE
-
-
-@app.route('/users/<int:_id>',methods=["PUT"])
-def update_user(_id):
-    body =request.json
-    valid_keys = {"name", "email"}
-    # check if there's any invalid key in the request body
-    if any(key not in valid_keys for key in body.keys()):
-        return jsonify({"error": "Invalid field in request body"}), 400  # return error message with 400 status code
-
-    for user in users_list:
-        if user['id'] == _id:
-            user.update(body)
-            return jsonify({
-                "message": f"user with id:{_id} updated"
-            })
-    return jsonify({
-        "message":"user not found"
-    })
-
-#DELETE
-@app.route('/users/<int:_id>',methods=["DELETE"])
-def delete_user(_id):
-    global users_list
-    flag = False
-    for user in users_list:
-        if user['id'] == _id:
-            flag = True
-    if flag == False:
-        return jsonify({
-        "message": f"user not found"
-    })
-    users_list = [user for user in users_list if user['id'] != _id]
-
-    resp = jsonify({
-        "message": f"id:{_id} deleted from users list"
-    })
-    return resp
+#
+# # UPDATE
+#
+#
+# @app.route('/users/<int:_id>',methods=["PUT"])
+# def update_user(_id):
+#     body =request.json
+#     valid_keys = {"name", "email","password"}
+#     # check if there's any invalid key in the request body
+#     if any(key not in valid_keys for key in body.keys()):
+#         return jsonify({"error": "Invalid field in request body"}), 400  # return error message with 400 status code
+#
+#
+#     return jsonify({
+#         "message":"user not found"
+#     })
+#
+# #DELETE
+# @app.route('/users/<int:_id>',methods=["DELETE"])
+# def delete_user(_id):
+#     global users_list
+#     flag = False
+#     for user in users_list:
+#         if user['id'] == _id:
+#             flag = True
+#     if flag == False:
+#         return jsonify({
+#         "message": f"user not found"
+#     })
+#     users_list = [user for user in users_list if user['id'] != _id]
+#
+#     resp = jsonify({
+#         "message": f"id:{_id} deleted from users list"
+#     })
+#     return resp
 
 if __name__ == "__main__":
     app.run(debug=True,port=3000)
