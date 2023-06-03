f5b5bcfb0ea64b9c7e22bdf25624d80a644fe2ee
Yarin Bukai <72891249+byarin90@users.noreply.github.com>
2023-06-03 15:47:11 +0300

    monog Exception work

--- app/main.py
+++ app/main.py
@@ -8,11 +8,13 @@ app = Flask(__name__)
 
 try:
     mongo = MongoClient(host="localhost",port=27018,serverSelectionTimeoutMS=5000)
+    # Attempt to fetch the server info. If this fails, an exception is raised
+    mongo.server_info()
     db = mongo.flask
-
     print("MongoDB Connect")
-except Exception:
+except Exception as e:  # This will catch any type of Exception
     print("Unable to connect MongoDB")
+    print(e)  # Print the error message
 
 
 
